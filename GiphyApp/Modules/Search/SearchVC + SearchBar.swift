14dca6a26c3abad550016a77f283ac8a78151aa2
Haydar Bekmuradov <haydarbekmuradov@mail.ru>
2022-03-03 19:17:39 +0300

    issue-1: Refactor project to MVVM (stage 3)

--- GiphyApp/Modules/Search/SearchVC + SearchBar.swift
+++ GiphyApp/Modules/Search/SearchVC + SearchBar.swift
@@ -16,8 +16,9 @@ extension SearchVC : UISearchBarDelegate {
         viewModel.propertyForSendSearchText = searchText
         timer.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false, block: { [self] timer in
-            
-            if searchText.isEmpty {viewModel.allGifsArray.removeAll()}
+            if searchText.isEmpty {
+                viewModel.allGifsArray.removeAll()
+            }
             self.viewModel.fetchGifs(searchQuery: searchText, offset: 0)
         })
         self.searchGifsCollectionView.reloadData()
